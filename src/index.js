24ff503ffc5539c9d7504d92527c5872ff2e3aff
Avetis Davydov <avetisdav94@gmail.com>
2023-09-25 20:51:43 +0200

    End config Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './module'
+import './scss/index.scss'
 console.log("Working...")
---
+++ src/module.js
@@ -0,0 +1,8 @@
+console.log("Module")
+
+
+async function start() {
+    await Promise.resolve()
+}
+
+start();
